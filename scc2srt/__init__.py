232625fe1c92a51ea5be6e24872532be215068cb
Dave Levinson <dlevinson5@gmail.com>
2017-01-25 19:43:37 -0500

    bug fix

--- scc2srt/__init__.py
+++ scc2srt/__init__.py
@@ -1,7 +1,8 @@
 import scc2srt.scc2srt
   
-def convert(input_file: str, output_file: str, logger: None):
- 
+def convert(input_file: str, output_file: str, alignment_padding = 0, logger: logging.Logger = None):
+
     items = scc2srt.parse(input_file, logger)
 
-    scc2srt.write_srt(items, output_file)
+    scc2srt.write_srt(items, alignment_padding, output_file)
+
