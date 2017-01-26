83ca061379a1998c18ad8fdd8fe868056dfe7765
Dave Levinson <dlevinson5@gmail.com>
2017-01-25 20:03:49 -0500

    bug fixes

--- scc2srt/__init__.py
+++ scc2srt/__init__.py
@@ -1,6 +1,7 @@
-import scc2srt.scc2srt
-  
-def convert(input_file: str, output_file: str, alignment_padding = 0, logger: logging.Logger = None):
+from . import scc2srt
+import logging
+
+def convert(input_file: str, output_file: str, alignment_padding=0, logger: logging.Logger = None):
 
     items = scc2srt.parse(input_file, logger)
 
