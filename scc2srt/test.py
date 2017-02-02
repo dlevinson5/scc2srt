14e6ca4bdff7a9a1015c3392e31926951f8dc05d
Dave Levinson <dlevinson5@gmail.com>
2017-02-02 12:19:09 -0500

    update

--- scc2srt/test.py
+++ scc2srt/test.py
@@ -19,13 +19,13 @@ if __name__ == "__main__":
 
     logger.addHandler(logHandler)
 
-    files = [f for f in os.listdir('C:\\Temp\\CAPTION') if re.match('.*\.scc', f)]
+    files = [f for f in os.listdir('Z:\\tmp\\_SRC') if re.match('.*.*\.scc', f)]
 
     for f in files:
-        input_file = '{}{}'.format('C:\\Temp\\CAPTION\\', f)
-        output_file = 'C:\\Temp\\CAPTION\\{}.srt'.format(os.path.splitext(f)[0])
+        input_file = '{}{}'.format('Z:\\tmp\\_SRC\\', f)
+        output_file = 'Z:\\tmp\\_OUTPUT\\{}.vtt'.format(os.path.splitext(f)[0])
         print(input_file)
-        items = scc2srt.parse(input_file, None, 3600)
+        items = scc2srt.parse(input_file, logger, 3600)
         scc2srt.write_srt(items, 0, output_file)
  
     print("done")
